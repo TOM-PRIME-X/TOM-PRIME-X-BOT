896ffd11d47dd8f7426e928fc473a37f33c30540
TOM-PRIME-X <saofficeal44@gmail.com>
2026-04-10 20:55:04 +0600

    Update simp.js

--- commands/simp.js
+++ commands/simp.js
@@ -52,8 +52,8 @@ async function simpCommand(sock, chatId, quotedMsg, mentionedJid, sender) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '',
+                    newsletterName: '𝐓𝐎𝐌 𝐏𝐑𝐈𝐌𝐄 𝐗',
                     serverMessageId: -1
                 }
             }
